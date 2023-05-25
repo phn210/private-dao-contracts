86ca23a879d921011e769f5d48547540757340df
Nam Phạm <nam.pv1805@gmail.com>
2023-05-25 10:27:48 +0700

    Add IDKGRequest.sol

---
+++ contracts/interfaces/IDKGRequest.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+interface IDKGRequest {
+    struct Request {
+        uint256 distributedKeyID;
+        uint256[][] R;
+        uint256[][] M;
+        uint256[] result;
+    }
+
+    /*================== EXTERNAL FUNCTION ==================*/
+    
+    function submitRequestResult(){
+        bytes32 _proposalID,
+        uint256[] calldata _result
+    } external;
+
+    /*==================== VIEW FUNCTION ====================*/
+    function getProposalID(
+        address _dao,
+        uint256 _distributedKeyID,
+        uint256 _timestamp
+    ) external view returns (bytes32);
+
+    function getRequest(
+        bytes32 _proposalID
+    ) external view returns (Request memory);
+
+    function getDistributedKeyID(
+        bytes32 _proposalID
+    ) external view returns (uint256);
+
+    function getR(
+        bytes32 _proposalID
+    ) external view returns (uint256[][] memory);
+
+    function getM(
+        bytes32 _proposalID
+    ) external view returns (uint256[][] memory);
+}
