9dd1a23663d5c56fc2c385064533dc95a4484a77
Nam Phạm <nam.pv1805@gmail.com>
2023-06-02 09:32:03 +0700

    Add script to generate distributed key

--- scripts/constants/address.ts
+++ scripts/constants/address.ts
@@ -1,1 +1,74 @@
-export const ADDRESSES: { [key: string]: { [key: string]: string } } = {};
+import { expect } from "chai";
+import { ethers, network } from "hardhat";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+export const ADDRESSES: { [key: string]: { [key: string]: string } } = {
+    31337: {
+        Round2ContributionVerifier:
+            "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        FundingVerifierDim3: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+        VotingVerifierDim3: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+        TallyContributionVerifierDim3:
+            "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+        ResultVerifierDim3: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+        PoseidonUnit2: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+        Poseidon: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+        FundManager: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+        DKG: "0x440C0fCDC317D69606eabc35C0F676D1a8251Ee1",
+    },
+};
+
+export async function loadAllContracts(
+    signer: SignerWithAddress,
+    chainID: string
+) {
+    let addresses = ADDRESSES[chainID];
+
+    return {
+        round2ContributionVerifier: await ethers.getContractAt(
+            "Round2ContributionVerifier",
+            addresses["Round2ContributionVerifier"],
+            signer
+        ),
+        fundingVerifierDim3: await ethers.getContractAt(
+            "FundingVerifierDim3",
+            addresses["FundingVerifierDim3"],
+            signer
+        ),
+        votingVerifierDim3: await ethers.getContractAt(
+            "VotingVerifierDim3",
+            addresses["VotingVerifierDim3"],
+            signer
+        ),
+        tallyContributionVerifierDim3: await ethers.getContractAt(
+            "TallyContributionVerifierDim3",
+            addresses["TallyContributionVerifierDim3"],
+            signer
+        ),
+        resultVerifierDim3: await ethers.getContractAt(
+            "ResultVerifierDim3",
+            addresses["ResultVerifierDim3"],
+            signer
+        ),
+        poseidonUnit2: await ethers.getContractAt(
+            "PoseidonUnit2",
+            addresses["PoseidonUnit2"],
+            signer
+        ),
+        poseidon: await ethers.getContractAt(
+            "Poseidon",
+            addresses["Poseidon"],
+            signer
+        ),
+        dkgContract: await ethers.getContractAt(
+            "DKG",
+            addresses["DKG"],
+            signer
+        ),
+        fundManager: await ethers.getContractAt(
+            "FundManager",
+            addresses["FundManager"],
+            signer
+        ),
+    };
+}
